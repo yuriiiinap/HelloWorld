21ecbc04360ae743168992c37ac07ae41ff060c1
g12952ty <g12952ty@s30452.tsuda.ac.jp>
2014-04-11 11:04:31 +0900

    HelloWorld!!

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g12952ty
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        // TODO code application logic here
+    }
+}
